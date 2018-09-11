3bdcef5158b7393d57952243f71b5aee0780089f
Scott Olsen <scg.olsen@gmail.com>
2018-09-11 08:46:31 -0400

    Use saner names

--- src/parser.lisp
+++ src/parser.lisp
@@ -2,8 +2,8 @@
 
 ;TODO: Factor out list conversion to a separate function.
 
-(defstruct (result)
-  "A parser result. Parsed and remaining contents."
+(defstruct (parser-result)
+  "A parser parser-result. Parsed and remaining contents."
   parsed 
   remaining)
 
@@ -11,8 +11,8 @@
   "Make a parser."
   (lambda (input)
     (let ((in (coerce input 'list)))
-    (cond ((funcall predicate (first in) sym) (make-result :parsed (first in) :remaining (rest in)))
-          (t (make-result :parsed nil :remaining (rest in)))))))
+    (cond ((funcall predicate (first in) sym) (make-parser-result :parsed (first in) :remaining (rest in)))
+          (t (make-parser-result :parsed nil :remaining (rest in)))))))
 
 (defun parser-fail ()
   "Enforce failure."
@@ -22,59 +22,44 @@
 (defun parser-return (sym)
   "Return a constant symbol."
   (lambda (input)
-    (make-result :parsed sym :remaining input)))
+    (make-parser-result :parsed sym :remaining input)))
 
-(defvar parser-a (gen-parser #'char= #\h))
-
-(funcall parser-a "hello")
-
-(defun parser-join (f)
-  (lambda (parser)
-    (funcall f (result-parsed ))))
-
-(defun sequential (p symbols) 
-  "Return a parser for all p symbols provided."
-  (map 'list ))
+(defun parser-join (&rest parsers)
+  (lambda (input)
+    ()))
 
 ;; Note this implementation of applicative is currently specific
 ;; to the parsing combinator structure.
-(defun applicative (fs xs) 
-  (labels ((interior (gs ys r) 
-             (if (null gs)
-                 (append (list (result-remaining (first r))) r)
-                 (interior (rest gs) (list (result-remaining (funcall (first gs) (first ys)))) (append (map 'list (first gs) ys) r))))) 
-    (reverse (interior fs xs nil))))
+(defun applicative (parsers tokens) 
+  (labels ((interior (parsers* tokens* result) 
+             (if (null parsers*)
+                 (append (list (parser-result-remaining (first result))) result)
+                 (interior (rest parsers*) (list (parser-result-remaining (funcall (first parsers*) (first tokens*)))) (append (map 'list (first parsers*) tokens*) result))))) 
+    (reverse (interior parsers tokens nil))))
 
 (defun partial (f &rest args)
   (lambda (&rest args1)
     (apply f (append args args1))))
 
-(defun sequential (p symbols)
+(defun sequential (predicate tokens)
   "Use an 'applicative interface' to combine parsers."
-  (let ((parsers (map 'list (partial #'gen-parser p) symbols)))
+  (let ((parsers (map 'list (partial #'gen-parser predicate) tokens)))
     (lambda (input) 
       (applicative parsers input))))
 
-(defun alternative (fs xs) 
-  (labels ((interior (gs ys r) 
-             (cond ((null gs) r)
-                   ((null (result-parsed (funcall (first gs) (first ys)))) (interior (rest gs) ys r))
-                   (t (interior gs (list (result-remaining (funcall (first gs) (first ys)))) (append (map 'list (first gs) ys) r))))))
-    (reverse (interior fs xs nil))))
+(defun alternative (parsers tokens) 
+  (labels ((interior (parsers* tokens* result) 
+             (cond ((null parsers*) result)
+                   ((null (parser-result-parsed (funcall (first parsers*) (first tokens*)))) (interior (rest parsers*) tokens* result))
+                   (t (interior parsers* (list (parser-result-remaining (funcall (first parsers*) (first tokens*)))) (append (map 'list (first parsers*) tokens*) result))))))
+    (reverse (interior parsers tokens nil))))
 
-(defun choice (p symbols)
+(defun choice (predicate tokens)
   "Prove a choice between parsing alternatives."
-  (let ((parsers (map 'list (partial #'gen-parser p) symbols)))
-    (lambda (x)
-     (alternative parsers x))))
+  (let ((parsers (map 'list (partial #'gen-parser predicate) tokens)))
+    (lambda (input)
+     (alternative parsers input))))
 
 (funcall (sequential #'char= '(#\h #\e #\l)) '("hello"))
-(funcall (applicative-combine parser-a parser-a (gen-parser #\l)) '("hhllo"))
 (funcall (choice #'char= '(#\h #\e #\l)) '("hello"))
-
-(defun stream-to-list () 
-  "Convert a stream to a list."
-  ())
-
-(coerce "hello" 'list)
-(with-input-from-string (s "hello") (coerce (read s) 'string))
+(funcall (parser-join (sequential #'char= '(#\h #\e #\l)) (choice #'char= '(#\o #\l))))
